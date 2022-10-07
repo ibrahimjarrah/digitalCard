3a21f792d54bbf022667cba37e69e1978ed8fe62
ibrahim jarrah <ibrahimjarrah@ibrahims-MacBook-Air.local>
2022-10-07 09:16:25 +0300

    digital BuissnessCard Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Imagess from "./image";
+import hemo1 from "./images/hemo.jpg";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img className="user1" src={hemo1} alt={"Ibrahim Jarrah"} />
+      <p className="name">Ibrahim Jarrah</p>
+      <p className="job">Full Stack Developer</p>
+      <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvkXlqjGncvBWPvHMqZnGjsWMFLsXNGsbKfgPwvNcSjwNQndfGKzQkQrjTLdsClmvLRQxV">
+        <button className="btn1">Email</button>
+      </a>
+      <a href="https://www.linkedin.com/in/ibrahim-jarrah-995b56153">
+        <button className="btn2">LinkedIn</button>
+      </a>
+      <p className="about">
+        <h1 className="first"> About</h1>
+        <p className="bdy">
+          Ibrahim Jarrah recently earned masters degree in computer and
+          communication engineering, with development skills especially in
+          ReactJs,NodeJs,CSS,HTML,and JS. Seeking an entry level position to
+          gain experience and be a part of your company.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </p>
+      <p className="skills">
+        <h1 className="seconed">Skills</h1>
+        <p className="bdy1">
+          <ul>
+            <li>Work under presssure</li>
+            <li>Team player and leader</li>
+            <li>Loyal,and creative</li>
+            <li>Time Management</li>
+          </ul>
+        </p>
+      </p>
+      <footer className="aboutMe">
+        <a href="https://www.facebook.com/ibrahim.jarrah.3">
+          <FaFacebook className="icon1" />
         </a>
-      </header>
+
+        <a href="https://www.linkedin.com/in/ibrahim-jarrah-995b56153">
+          <FaLinkedin className="icon2" />
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import hemo1 from "./images/hemo.jpg";
+
+function Imagess() {
+  return (
+    <div>
+      <img src={hemo1} />
+    </div>
+  );
+}
+
+export default Imagess;
